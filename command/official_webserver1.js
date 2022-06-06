e07884ac26bfab1e8a240560b74b03350567c549
ZiyadA16 <84078683+ZiyadA16@users.noreply.github.com>
2022-06-06 16:14:59 +0100

    new

--- command/official_webserver1.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var server = express();
-
-server.get('/', function(req, res) {
-    res.sendFile('/home/ubuntu/drone_screen.html')
-})
-
-console.log('Server is running on port 3000'); 
-server.listen(3000,'0.0.0.0');
