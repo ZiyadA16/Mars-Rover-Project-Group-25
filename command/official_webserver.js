a48d82aff75e7a84ac0d607e0141fc60876925b6
rana2302 <67081360+rana2302@users.noreply.github.com>
2022-06-07 16:19:10 +0100

    Add files via upload

--- command/official_webserver.js
+++ command/official_webserver.js
@@ -3,22 +3,29 @@ var server = express();
 
 
 server.get('/', function(req, res) {
-    res.sendFile('/home/ubuntu/command/rover_mainpage.html'); 
+    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
 });
 
-server.get('/', function(req, res) {
-    res.sendFile('/home/ubuntu/command/rover_screen.html'); 
+server.get('/rover_mainpage.html', function(req, res) {
+    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
 });
 
-  server.get('/', function(req, res) {
-      res.sendFile('/home/ubuntu/command/rover_map.html'); 
-  });
+server.get('/rover_explore.html', function(req, res) {
+    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_explore.html'); 
+});
 
- server.get('/', function(req, res) {
-     res.sendFile('/home/ubuntu/command/rover_about.html'); 
- });
+server.get('/rover_map.html', function(req, res) {
+    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_map.html'); 
+});
 
-
+server.get('/rover_about.html', function(req, res) {
+    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_about.html'); 
+});
 
 console.log('Server is running on port 3000'); 
 server.listen(3000,'0.0.0.0');
