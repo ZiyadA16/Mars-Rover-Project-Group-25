47aad70e64726efbad6be0558c2fed22272e077a
ZiyadA16 <84078683+ZiyadA16@users.noreply.github.com>
2022-06-08 00:05:22 +0100

    new

--- command/official_webserver.js
+++ command/official_webserver.js
@@ -1,30 +1,38 @@
 var express = require('express');
+const { appendFile } = require('fs');
 var server = express();
 
 
 server.get('/', function(req, res) {
-    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
-    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    res.sendFile('/home/ubuntu/command/rover_mainpage.html');
+    res.sendFile('/home/ubuntu/command/rover_explore.html');
+    res.sendFile('/home/ubuntu/command/Roboto-Light.ttf');
+    res.sendFile('/home/ubuntu/command/Astronomus.ttf');
+    res.sendFile('/home/ubuntu/command/foreign-alien.ttf');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
 });
 
-server.get('/rover_mainpage.html', function(req, res) {
-    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
-    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+
+server.use('/static', express.static('/home/ubuntu/command/Astronomus.ttf'));
+
+    server.get('/rover_mainpage.html', function(req, res) {
+    res.sendFile('/home/ubuntu/command/rover_mainpage.html');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
 });
 
 server.get('/rover_explore.html', function(req, res) {
-    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
-    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_explore.html'); 
+    res.sendFile('/home/ubuntu/command/rover_mainpage.html');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_explore.html'); 
 });
 
 server.get('/rover_map.html', function(req, res) {
-    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
-    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_map.html'); 
+    res.sendFile('/home/ubuntu/command/rover_mainpage.html');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_map.html'); 
 });
 
 server.get('/rover_about.html', function(req, res) {
-    //res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
-    res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_about.html'); 
+    res.sendFile('/home/ubuntu/command/rover_mainpage.html');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_about.html'); 
 });
 
 console.log('Server is running on port 3000'); 
