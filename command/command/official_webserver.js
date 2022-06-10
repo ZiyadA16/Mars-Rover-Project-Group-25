1997b65cbbb2a6fb47a0b7423ebff7acbf65b4d5
rana2302 <67081360+rana2302@users.noreply.github.com>
2022-06-10 17:31:21 +0100

    Add files via upload

---
+++ command/command/official_webserver.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var server = express();
+
+server.get('/', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+});
+
+server.get('/rover_mainpage.html', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_mainpage.html'); 
+});
+
+server.get('/rover_explore.html', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_explore.html'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_explore.html'); 
+});
+
+server.get('/rover_map.html', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_map.html'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_map.html'); 
+});
+
+server.get('/rover_about.html', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/rover_about.html'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/rover_about.html'); 
+});
+
+server.get('/foreign-alien.ttf', function(req, res) {
+     res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/foreign-alien.ttf'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/foreign-alien.ttf'); 
+});
+
+server.get('/Astronomus.ttf', function(req, res) {
+     res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/Astronomus.ttf'); 
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/Astronomus.ttf'); 
+});
+
+server.get('/Roboto-Light.ttf', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/Roboto-Light.ttf'); 
+    //res.sendFile('/home/ubuntu/command/Roboto-Light.ttf');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/Roboto-Light.ttf'); 
+});
+
+server.get('/mars_pic.jpg', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/mars_pic.jpg'); 
+    //res.sendFile('/home/ubuntu/command/mars_pic.jpg');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/Roboto-Light.ttf'); 
+});
+
+server.get('/mars_pic2.jpg', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/mars_pic2.jpg');
+    //res.sendFile('/home/ubuntu/command/mars_pic2.jpg');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/Roboto-Light.ttf'); 
+});
+
+server.get('/nightsky.jpeg', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/nightsky.jpeg');
+    //res.sendFile('/home/ubuntu/command/nightsky.jpeg');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/nightsky.jpeg'); 
+});
+
+server.get('/about_page.png', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/about_page.png');
+    //res.sendFile('/home/ubuntu/command/about_page.png');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/about_page.png'); 
+});
+
+server.get('/Roverium.png', function(req, res) {
+    res.sendFile('/home/ubuntu/Mars-Rover-Project-Group-25-main 2/command/Roverium.png'); 
+    //res.sendFile('/home/ubuntu/command/Roverium.png');
+    //res.sendFile('/Users/aryanrana/Desktop/Mars-Rover-Project-Group-25-main 2/command/about_page.png'); 
+});
+
+
+
+console.log('Server is running on port 3000'); 
+server.listen(3000,'0.0.0.0');
