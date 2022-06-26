aab51b51a0e11957b6c3ac6458583a611207d2c3
lc2920 <94857891+lc2920@users.noreply.github.com>
2022-06-26 11:59:58 +0100

    filter

--- Vision/IMGPROC.v
+++ Vision/IMGPROC.v
@@ -79,7 +79,7 @@ wire         sop, eop, in_valid, out_ready;
 //HSV Conversion Variable Declaration:
 wire [7:0] hue, saturation, value, cmax, cmin;
 
-//HSV Conversion:
+//HSV Conversion: (this is for decimal values, not binary) -> need to consider whether i should divide by 255, multiply by 100 etc or not!
 assign cmax = (blue > green) ? ((blue > red) ? blue[7:0] : red[7:0]) : (green > red) ? green [7:0] : red[7:0];
 assign cmin = (blue < green) ? ((blue < red) ? blue[7:0] : red[7:0]) : (green < red) ? green [7:0] : red[7:0];
 assign hue = (cmax == cmin) ? 0 
@@ -90,8 +90,9 @@ assign saturation = (cmax == 0) ? 0 : ((cmax - cmin)* 100 / cmax); // 0 to 100%
 assign value = (cmax); //0 to 255
 
 
+//Detect Ping Pong Balls: (Only green for now) (Filter declaration too)
 //Detect Ping Pong Balls:
-wire red_detect, violet_detect, blue_detect, orange_detect, /*pink_detect,*/ yellow_detect, lime_detect, teal_detect, white_detect;
+wire red_detect, violet_detect, blue_detect, orange_detect, /*pink_detect,*/ yellow_detect, lime_detect, teal_detect, white_detect, black_detect;
 
 //assign violet_detect = (hue >= 120 && hue <= 140) && (saturation > 40 && saturation < 60 && value >= 115 );  //>270  <280
 //assign orange_detect = (hue >= 25 && hue <= 35); /*&& (saturation > 40 && saturation < 100 && value >= 15 && value <= 70)) 70,50*/
@@ -103,14 +104,32 @@ assign teal_detect = (hue >= 39 && hue  <= 90) && (saturation > 38 && saturation
 //assign pink_detect = 0; //(hue >= 2 && hue <= 25) && (saturation > 60 && saturation <= 100 && value >= 83);
 assign yellow_detect = (hue >= 10 && hue <= 50) && (saturation > 58 && saturation <= 100 && value >= 65); //hue , sat , val
 assign lime_detect = (hue >= 38  && hue <= 73) && (saturation > 22 && saturation <= 91 && value >= 68);
-assign white_detect = (value >= 250);
+assign white_detect = (saturation <= 2 && value >= 250);
+assign black_detect = (value <= 10);
 
+
+/*
+H 150 180
+s 40 80
+v 50 80 */
 
 /* Detect red areas (using rgb)
 wire red_detect;
 assign red_detect = red[7] & ~green[7] & ~blue[7]; */
 
 // Find boundary of cursor box
+
+/*Filter*/
+reg prev_v, prev_v1, prev_v2;
+reg prev_b, prev_b1, prev_b2;
+reg prev_r, prev_r1, prev_r2;
+reg prev_t, prev_t1, prev_t2;
+reg prev_y, prev_y1, prev_y2;
+reg prev_l, prev_l1, prev_l2;
+reg prev_w, prev_w1, prev_w2;
+reg prev_bl, prev_bl1, prev_bl2;
+
+
 
 /* Highlight detected areas
 wire [23:0] red_high;
@@ -119,26 +138,6 @@ assign red_high  =  red_detect ? {8'hff, 8'h0, 8'h0} : {grey, grey, grey}; */
 wire [23:0] highlight;
 assign grey = green[7:1] + red[7:2] + blue[7:2];
 //assign highlight = (orange_detect || red_detect || violet_detect || blue_detect) ? {8'h04,8'hbd,8'h42} : {grey, grey, grey};
-assign highlight = 
-		  (red_detect) ? {8'hff, 8'h0, 8'h0}
-		: (blue_detect) ? {8'h0,8'h0,8'hff}
-		: (yellow_detect) ? {8'hff,8'hff,8'h0} 
-		: (teal_detect) ? {8'h0,8'h80,8'h80}
-		: (lime_detect) ? {8'h32,8'hcd,8'h32} 
-		//: (pink_detect) ? {8'hff,8'hc0,8'cb}
-		: (white_detect) ? {8'hff,8'hff,8'hff} 
-		: {grey, grey, grey};
-		
-
-//(red_detect || teal_detect || pink_detect || blue_detect || yellow_detect || lime_detect) ? {8'h04,8'hbd,8'h42} : {grey, grey, grey};
-
-//red:{8'hff, 8'h0, 8'h0}
-//blue:{8'h0,8'h0,8'hff}
-//teal:{8'h0,8'h80,8'h80} 
-//pink:{8'hff,8'hc0,8'cb} 
-//yellow:{8'hff,8'hff,8'h0} 
-//lime:{8'h32,8'hcd,8'h32} 
-//white:{8'hff,8'hff,8'hff} 
 
 
 // Show bounding box
@@ -151,6 +150,81 @@ assign new_image = bb_active ? bb_col : highlight;
 // Don't modify the start-of-packet word - it's a packet discriptor
 // Don't modify data in non-video packets
 assign {red_out, green_out, blue_out} = (mode & ~sop & packet_video) ? new_image : {red,green,blue};
+
+initial begin 
+	prev_v<=0;
+	prev_v1<=0;
+	prev_v2<=0;
+	prev_b<=0;
+	prev_b1<=0;
+	prev_b2<=0;
+	prev_r<=0;
+	prev_r1<=0;
+	prev_r2<=0;
+	prev_t<=0;
+	prev_t1<=0;
+	prev_t2<=0;
+	prev_y<=0;
+	prev_y1<=0;
+	prev_y2<=0;
+	prev_l<=0;
+	prev_l1<=0;
+	prev_l2<=0;
+	prev_w<=0;
+	prev_w1<=0;
+	prev_w2<=0;
+	prev_bl<=0;
+	prev_bl1<=0;
+	prev_bl2<=0;
+end
+
+always@(negedge clk) begin
+/*
+	prev_violet = violet_detect;
+	prev_violet1 = prev_violet;
+	prev_violet2 = prev_violet1;
+	*/
+	prev_b2 = prev_b1;
+	prev_b1 = prev_b;
+	prev_b = blue_detect;	
+	
+	prev_r2 = prev_r1;
+	prev_r1 = prev_r;
+	prev_r = red_detect;	
+	
+	prev_t2 = prev_t1;
+	prev_t1 = prev_t;
+	prev_t = teal_detect;
+	
+	prev_y2 = prev_y1;
+	prev_y1 = prev_y;
+	prev_y = yellow_detect;
+	
+	prev_l2 = prev_l1;
+	prev_l1 = prev_l;
+	prev_l = lime_detect;
+	
+	prev_w2 = prev_w1;
+	prev_w1 = prev_w;
+	prev_w = white_detect;
+	
+	prev_bl2 = prev_bl1;
+	prev_bl1 = prev_bl;
+	prev_bl = black_detect;
+	
+	
+end
+
+//(violet_detect && prev_violet && prev_violet1 && prev_violet2) ? {8'h04,8'hbd,8'h42} 
+assign highlight = (red_detect && prev_r && prev_r1 && prev_r2) ? {8'hec,8'h42,8'h27} 
+: ((blue_detect && prev_b && prev_b1 && prev_b2) ? {8'h04,8'h48,8'hd4}
+: ((teal_detect && prev_t && prev_t1 && prev_t2) ? {8'h36,8'hcb,8'hff}
+: ((yellow_detect && prev_y && prev_y1 && prev_y2) ? {8'he3,8'hd5,8'h09}
+: ((lime_detect && prev_l && prev_l1 && prev_l2) ? {8'h36,8'hcb,8'hff}
+: ((white_detect && prev_w && prev_w1 && prev_w2) ? {8'hff,8'hff,8'hff}
+: ((black_detect && prev_bl && prev_bl1 && prev_bl2) ? {8'hff,8'hff,8'hff}
+: {grey, grey, grey}))))));
+
 
 //Count valid pixels to get the image coordinates. Reset and detect packet type on Start of Packet.
 reg [10:0] x, y;
@@ -173,9 +247,9 @@ always@(posedge clk) begin
 end
 
 //Find first and last red pixels
-reg [10:0] x_min, y_min, x_max, y_max;
+reg [10:0] x_min, x_max, y_min, y_max;
 always@(posedge clk) begin
-	if ((teal_detect || red_detect || /*pink_detect ||*/ blue_detect || yellow_detect || lime_detect || white_detect)& in_valid) begin	//Update bounds when the pixel is red
+	if ((red_detect)& in_valid) begin	//Update bounds when the pixel is red
 		if (x < x_min) x_min <= x;
 		if (x > x_max) x_max <= x;
 		if (y < y_min) y_min <= y;
@@ -188,6 +262,89 @@ always@(posedge clk) begin
 		y_max <= 0;
 	end
 end
+/*
+reg [10:0] x_min_b, x_max_B;
+always@(posedge clk) begin
+	if ((red_detect)& in_valid) begin	//Update bounds when the pixel is blue
+		if (x < x_min) x_min <= x;
+		if (x > x_max) x_max <= x;
+		if (y < y_min) y_min <= y;
+		y_max <= y;
+	end
+	if (sop & in_valid) begin	//Reset bounds on start of packet
+		x_min <= IMAGE_W-11'h1;
+		x_max <= 0;
+		y_min <= IMAGE_H-11'h1;
+		y_max <= 0;
+	end
+end
+
+reg [10:0] x_min, x_max;
+always@(posedge clk) begin
+	if ((red_detect)& in_valid) begin	//Update bounds when the pixel is teal
+		if (x < x_min) x_min <= x;
+		if (x > x_max) x_max <= x;
+		if (y < y_min) y_min <= y;
+		y_max <= y;
+	end
+	if (sop & in_valid) begin	//Reset bounds on start of packet
+		x_min <= IMAGE_W-11'h1;
+		x_max <= 0;
+		y_min <= IMAGE_H-11'h1;
+		y_max <= 0;
+	end
+end
+
+reg [10:0] x_min, x_max;
+always@(posedge clk) begin
+	if ((red_detect)& in_valid) begin	//Update bounds when the pixel is yellow
+		if (x < x_min) x_min <= x;
+		if (x > x_max) x_max <= x;
+		if (y < y_min) y_min <= y;
+		y_max <= y;
+	end
+	if (sop & in_valid) begin	//Reset bounds on start of packet
+		x_min <= IMAGE_W-11'h1;
+		x_max <= 0;
+		y_min <= IMAGE_H-11'h1;
+		y_max <= 0;
+	end
+end
+
+reg [10:0] x_min, x_max;
+always@(posedge clk) begin
+	if ((red_detect)& in_valid) begin	//Update bounds when the pixel is lime
+		if (x < x_min) x_min <= x;
+		if (x > x_max) x_max <= x;
+		if (y < y_min) y_min <= y;
+		y_max <= y;
+	end
+	if (sop & in_valid) begin	//Reset bounds on start of packet
+		x_min <= IMAGE_W-11'h1;
+		x_max <= 0;
+		y_min <= IMAGE_H-11'h1;
+		y_max <= 0;
+	end
+end
+
+reg [10:0] x_min, x_max;
+always@(posedge clk) begin
+	if ((red_detect)& in_valid) begin	//Update bounds when the pixel is white/black
+		if (x < x_min) x_min <= x;
+		if (x > x_max) x_max <= x;
+		if (y < y_min) y_min <= y;
+		y_max <= y;
+	end
+	if (sop & in_valid) begin	//Reset bounds on start of packet
+		x_min <= IMAGE_W-11'h1;
+		x_max <= 0;
+		y_min <= IMAGE_H-11'h1;
+		y_max <= 0;
+	end
+end
+*/
+//Drive & Distance Instr
+
 
 
 
@@ -362,3 +519,4 @@ assign msg_buf_rd = s_chipselect & s_read & ~read_d & ~msg_buf_empty & (s_addres
 -Autonomous drive instructions? */
 
 endmodule
+
